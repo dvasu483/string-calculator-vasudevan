97a00c2a50bd409617012743f818eaa3e33e1a20
dhinakaran <vasudevan.dhinakaran-ee@infineon.com>
2025-03-28 08:37:22 +0530

    Handled single number string

--- src/utils/stringCalculator.test.js
+++ src/utils/stringCalculator.test.js
@@ -3,3 +3,7 @@ import { add } from './stringCalculator';
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test ("returns the number for a single number string", () => {
+    expect(add("5")).toBe(5);
+});
